101b8f1fe0da591e81b719d64da9f35d08290eb8
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-22 16:19:08 +0300

    Add Logger Middleware to log Http Requests Fix autogenerated tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, UsersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
