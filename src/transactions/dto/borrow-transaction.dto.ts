015b29185d8edf89edc0cac4c7b55375cfda1126
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-23 00:09:19 +0300

    Add IsAfter validation decorator to validate that dueDate and returnedAt is after borrowedAt

--- src/transactions/dto/borrow-transaction.dto.ts
+++ src/transactions/dto/borrow-transaction.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsDate, IsInt, IsOptional } from 'class-validator';
+import { IsAfter } from '../../utils/dtos';
 
 export class BorrowTransactionDto {
   @ApiProperty({
@@ -38,10 +39,11 @@ export class BorrowTransactionDto {
     description: 'The date the book is returned',
     required: false,
   })
+  @IsAfter('borrowedAt')
   @IsDate()
   @Type(() => Date)
   @IsOptional()
-  returnDate?: Date;
+  returnedAt?: Date;
 
   @ApiProperty({
     type: Date,
@@ -49,12 +51,13 @@ export class BorrowTransactionDto {
     default: '2024-05-22',
     required: true,
   })
+  @IsAfter('borrowedAt')
   @IsDate()
   @Type(() => Date)
   dueDate: Date;
 }
 
 export class CreateBorrowTransactionDto extends OmitType(BorrowTransactionDto, [
-  'returnDate',
+  'returnedAt',
   'bookId',
 ] as const) {}
--- src/utils/dtos.ts
+++ src/utils/dtos.ts
@@ -1,6 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt } from 'class-validator';
+import {
+  IsInt,
+  ValidateBy,
+  ValidationArguments,
+  ValidationOptions,
+  buildMessage,
+} from 'class-validator';
 
 export class FindByIdParamsDto {
   @ApiProperty({
@@ -12,3 +18,37 @@ export class FindByIdParamsDto {
   @Type(() => Number)
   id: number;
 }
+
+// **************** Decorators **************** //
+
+/**
+ * Decorator function that validates if a date property is after another date property.
+ * @param property - The name of the related date property.
+ * @param options - Optional validation options.
+ * @returns A property decorator function.
+ */
+export const IsAfter = (
+  property: string,
+  options?: ValidationOptions,
+): PropertyDecorator =>
+  ValidateBy(
+    {
+      name: 'IsAfter',
+      constraints: [property],
+      validator: {
+        validate: (value: Date, args: ValidationArguments): boolean => {
+          const [relatedPropertyName] = args.constraints;
+          const relatedValue = (args.object as Record<string, unknown>)[
+            relatedPropertyName
+          ] as Date;
+          return value.toISOString() > relatedValue.toISOString();
+        },
+        defaultMessage: buildMessage(
+          (each: string): string =>
+            each + '$property must be after $constraint1',
+          options,
+        ),
+      },
+    },
+    options,
+  );
