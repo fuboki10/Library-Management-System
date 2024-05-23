8403212182e33e4b20ed632856c20b3646e94333
fuboki10 <abdelrahman.tarek1910@gmail.com>
2024-05-24 01:56:55 +0300

    Fix tests

--- src/analytics/analytics.controller.spec.ts
+++ src/analytics/analytics.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticsService } from './analytics.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('AnalyticsController', () => {
   let controller: AnalyticsController;
@@ -8,7 +9,7 @@ describe('AnalyticsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AnalyticsController],
-      providers: [AnalyticsService],
+      providers: [AnalyticsService, PrismaService],
     }).compile();
 
     controller = module.get<AnalyticsController>(AnalyticsController);
